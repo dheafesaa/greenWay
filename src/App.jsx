c6d6f4b253df2fbf9a9e76e8305f9655db370eef
Dhea Fesa Athallah <dhea.athallah@ist.id>
2024-12-30 23:44:53 +0700

    implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { asyncUnsetAuthUser } from './states/authUser/action';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import Navbar from './components/organisms/Navbar';
 
 function App() {
+  const {
+    authUser = null,
+    isPreload = false,
+  } = useSelector((states) => states);
+
+  const location = useLocation();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPreloadProcess());
+  }, [dispatch]);
+
+  const onSignOut = () => {
+    dispatch(asyncUnsetAuthUser());
+  };
+
+  if (isPreload) {
+    return null;
+  }
+
+  const isLoginOrRegisterPage = location.pathname === '/login' || location.pathname === '/register';
+
   return (
     <div className="app">
+       <header>
+        {!isLoginOrRegisterPage && <Navbar authUser={authUser} signOut={onSignOut} />}
+      </header>
       <main>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
