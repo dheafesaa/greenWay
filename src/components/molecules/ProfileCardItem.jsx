361ad0f4bd3370762d640783ed847cc0e4a32520
Dhea Fesa Athallah <dhea.athallah@ist.id>
2025-01-04 15:23:05 +0700

    fixing profile card

--- src/components/molecules/ProfileCardItem.jsx
+++ src/components/molecules/ProfileCardItem.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-import { AiOutlineLinkedin } from 'react-icons/ai';
-import { VscGithub } from 'react-icons/vsc';
-import { MdOutlineMail } from 'react-icons/md';
-import IconButton from '../atoms/IconButton';
-
-function ProfileCardItem({
-  photo, name, role1, role2, linkedin, github, email,
-}) {
+function ProfileCardItem({ photo, name, role1, role2 }) {
   return (
     <Card sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
       <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -20,11 +12,6 @@ function ProfileCardItem({
         <Typography variant="body1" fontWeight="600">{name}</Typography>
         <Typography variant="body1">{role1}</Typography>
         <Typography variant="body1">{role2}</Typography>
-        <Box display="flex" gap={2} mt={2}>
-          <IconButton to={linkedin} icon={AiOutlineLinkedin} />
-          <IconButton to={github} icon={VscGithub} />
-          <IconButton to={`mailto:${email}`} icon={MdOutlineMail} />
-        </Box>
       </CardContent>
     </Card>
   );
@@ -36,9 +23,6 @@ const profileCardItemShape = {
   name: PropTypes.string.isRequired,
   role1: PropTypes.string.isRequired,
   role2: PropTypes.string.isRequired,
-  linkedin: PropTypes.string.isRequired,
-  github: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
 };
 
 ProfileCardItem.propTypes = {
