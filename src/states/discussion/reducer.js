d1d1c066197f02082aae1b7494986e14df0188a6
Dhea Fesa Athallah <dhea.athallah@ist.id>
2025-01-08 22:26:44 +0700

    setup redux action for discussion

--- src/states/discussion/reducer.js
+++ src/states/discussion/reducer.js
@@ -1,4 +1,4 @@
-import { ActionType } from './action';
+import { ActionType } from "./action";
 
 const initialState = {
   discussions: [],
@@ -17,6 +17,75 @@ const discussionReducer = (state = initialState, action = {}) => {
         ...state,
         discussion: action.payload.data.discussion,
       };
+    case ActionType.TOGGLE_LIKE_DISCUSSION:
+      return discussions.map((discussion) => {
+        if (discussion.id === action.payload.discussionId) {
+          const updatedUpVotesBy = discussion.upVotesBy.includes(
+            action.payload.userId,
+          )
+            ? discussion.upVotesBy.filter((id) => id !== action.payload.userId)
+            : [
+                ...discussion.upVotesBy.filter(
+                  (id) => id !== action.payload.userId,
+                ),
+                action.payload.userId,
+              ];
+
+          const updatedDownVotesBy = discussion.downVotesBy.filter(
+            (id) => id !== action.payload.userId,
+          );
+
+          return {
+            ...discussion,
+            upVotesBy: updatedUpVotesBy,
+            downVotesBy: updatedDownVotesBy,
+          };
+        }
+        return discussion;
+      });
+    case ActionType.TOGGLE_UNLIKE_DISCUSSION:
+      return discussions.map((discussion) => {
+        if (discussion.id === action.payload.discussionId) {
+          const updatedDownVotesBy = discussion.downVotesBy.includes(
+            action.payload.userId,
+          )
+            ? discussion.downVotesBy.filter(
+                (id) => id !== action.payload.userId,
+              )
+            : [
+                ...discussion.downVotesBy.filter(
+                  (id) => id !== action.payload.userId,
+                ),
+                action.payload.userId,
+              ];
+
+          const updatedUpVotesBy = discussion.upVotesBy.filter(
+            (id) => id !== action.payload.userId,
+          );
+
+          return {
+            ...discussion,
+            upVotesBy: updatedUpVotesBy,
+            downVotesBy: updatedDownVotesBy,
+          };
+        }
+        return discussion;
+      });
+    case ActionType.TOGGLE_NEUTRALIZE_DISCUSSION:
+      return discussions.map((discussion) => {
+        if (discussion.id === action.payload.discussionId) {
+          return {
+            ...discussion,
+            upVotesBy: discussion.upVotesBy.filter(
+              (id) => id !== action.payload.userId,
+            ),
+            downVotesBy: discussion.downVotesBy.filter(
+              (id) => id !== action.payload.userId,
+            ),
+          };
+        }
+        return discussion;
+      });
     default:
       return state;
   }
