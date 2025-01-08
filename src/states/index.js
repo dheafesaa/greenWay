f6578329e7e6b2274cab8fe763a5a0abdc5042d9
Dhea Fesa Athallah <dhea.athallah@ist.id>
2025-01-08 13:33:04 +0700

    add photo member

--- src/states/index.js
+++ src/states/index.js
@@ -4,7 +4,6 @@ import authUserReducer from './authUser/reducer';
 import usersReducer from './users/reducer';
 import loadingReducer from './loading/reducer';
 import articlesReducer from './articles/reducer';
-import aboutUsReducer from './aboutUS/reducer';
 import destinationReducer from './destinations/reducer';
 import campaignReducer from './campaign/reducer';
 import discussionReducer from './discussion/reducer';
@@ -16,7 +15,6 @@ const store = configureStore({
     users: usersReducer, 
     loading: loadingReducer,
     articles: articlesReducer,
-    aboutUs: aboutUsReducer,
     destination: destinationReducer,
     campaign: campaignReducer,
     discussion: discussionReducer,
--- src/utils/data.js
+++ src/utils/data.js
@@ -25,6 +25,42 @@ export const purposeCards = [
   },
 ];
 
+import dheaPhoto from '../assets/dhea.jpg';
+import fransiskaPhoto from '../assets/fransiska.jpeg';
+import allanPhoto from '../assets/allan.jpeg';
+import yogaPhoto from '../assets/yoga.jpg';
+
+export const profileData = [
+  {
+    id: 1,
+    name: 'Dhea Fesa Athallah',
+    nim: '21570002',
+    prodi: 'Teknik Informatika',
+    photo: dheaPhoto
+  },
+  {
+    id: 2,
+    name: 'Fransiska Warkop Anjali',
+    nim: '21560020',
+    prodi: 'Sistem Informasi',
+    photo: fransiskaPhoto
+  },
+  {
+    id: 3,
+    name: 'Allan Yuliansyah',
+    nim: '24565004',
+    prodi: 'Sistem Informasi',
+    photo: allanPhoto
+  },
+  {
+    id: 4,
+    name: 'Yoga Wanda Pratama',
+    nim: '24575001',
+    prodi: 'Teknik Informatika',
+    photo: yogaPhoto
+  },
+];
+
 export const logoGreenWay = 'https://firebasestorage.googleapis.com/v0/b/mostgreen.appspot.com/o/Tak_berjudul63-hd__2_-removebg-preview.png?alt=media&token=eca5f180-7753-4567-94a5-6ed13f674861';
 
 export const imgProduct = 'https://firebasestorage.googleapis.com/v0/b/mostgreen.appspot.com/o/Taking%20care%20of%20the%20Earth-bro.png?alt=media&token=be3278b6-c9bb-4b77-a9d0-91dc49c20cfa';
